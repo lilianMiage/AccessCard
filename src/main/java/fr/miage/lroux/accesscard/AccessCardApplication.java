199ef0b008b8c780cf4046c73557d3469e9c9606
Lilianroux <lilianroux02@gmail.com>
2025-05-27 14:40:20 +0200

    first commit

---
+++ src/main/java/fr/miage/lroux/accesscard/AccessCardApplication.java
@@ -0,0 +1,13 @@
+package fr.miage.lroux.accesscard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessCardApplication.class, args);
+	}
+
+}
